9a21b73e184331949bca0193fe510602bde1e6fe
Dennis Chen <chensuyuan1989@hotmail.com>
2022-07-11 00:46:16 -0400

    processing Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { Link } from "react-router-dom";
-import 
+import { ImMenu } from "react-icons/im";
 
 const NavBar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const sideButtonRef = useRef(null);
+  const linkRef = useRef(null);
+
+  const toggleButton = () => {
+    sideButtonRef.current.style.transform = `${
+      showLinks ? "" : "rotate(90deg)"
+    }`;
+  };
+
+  const toggleLink = () => {
+    linkRef.current.style.display = `${showLinks ? "block" : ""}`;
+  };
+
   return (
-    <nav className="nav-container">
-      <div className="nav-sidebar"></div>
-      <div className="nav-title">
-        <Link to="/">
-          <h3>Hi, I'm Dennis Chen</h3>
-        </Link>
-      </div>
-      <div className="nav-links">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/project">Projects</Link>
+    <nav>
+      <div className="nav-container">
+        <div className="nav-header">
+          <div className="nav-sidebar">
+            <button
+              ref={sideButtonRef}
+              className="sideBtn"
+              onClick={() => {
+                setShowLinks(!showLinks);
+                toggleLink();
+                toggleButton();
+              }}
+            >
+              <ImMenu />
+            </button>
+          </div>
+          <div className="nav-title">
+            <Link to="/">
+              <h3>Hi, I'm Dennis Chen</h3>
+            </Link>
+          </div>
+        </div>
+        <div className="link-container">
+          <ul className="nav-links" ref={linkRef}>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/project">Projects</Link>
+          </ul>
+        </div>
       </div>
     </nav>
   );
